cfe19b11bca0fa94e620040d887fc19c09bf053d
Serra, Albert | Albert | BDD <albert.serra@rakuten.com>
2016-01-27 16:55:43 +0900

    Test login

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
+  if(loginKey){
+  	return response.end("You need to login.\n");
+  }
   response.end("Hello World\n");
 });
 
 var port = (process.env.PORT || 8000);
-
+var loginKey = (process.env.LoginKey);
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(port);
 
